5e5b50a8d1cccdd5799380dfd2fbfe9cc6479c1d
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-07-24 22:32:05 +0100

    Fixed indentation

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -28,78 +28,78 @@ public class BubbleSort
     	    	sorted = true;
     	    	for(int j = 0; j < array.length - i - 1; j++)
     	    	{
-    	    		if(array[j] > array[j + 1]) 
+    	    	    if(array[j] > array[j + 1]) 
     	    	    { 
-    	    			//Array is not sorted as swap was needed
-    	    			sorted = false;
+    	    		    //Array is not sorted as swap was needed
+    	    		    sorted = false;
 
-    	    			//Swaps array[j + 1] and array[j] 
-    	    			int temp = array[j]; 
-    	    			array[j] = array[j + 1]; 
-    	    			array[j + 1] = temp; 
+    	    		    //Swaps array[j + 1] and array[j] 
+    	    		    int temp = array[j]; 
+    	    		    array[j] = array[j + 1]; 
+    	    		    array[j + 1] = temp; 
     	    	    }
     	    	}
     	    }
     	}
     }
     
-	/**
+    /**
      * Sorts the specified array of doubles in ascending order using bubble sort.
      * 
      * @param array the unsorted array of doubles
      */
     public static void sortDoubles(double[] array)
     {
-       	if(array != null)
-    	{
-    		boolean sorted = false;
-	        for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
-	        {
-	        	sorted = true;
-	            for(int j = 0; j < array.length - i - 1; j++)
-	            {
-	                if(array[j] > array[j + 1]) 
-	                { 
-	                	//Array is not sorted as swap was needed
-	                	sorted = false;
-	                	
-	                	//Swaps array[j + 1] and array[j] 
-	                    double temp = array[j]; 
-	                    array[j] = array[j + 1]; 
-	                    array[j + 1] = temp; 
-	                }
-	            }
-	        }
+        if(array != null)
+        {
+    	    boolean sorted = false;
+    	    for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
+    	    {
+    	    	sorted = true;
+    	    	for(int j = 0; j < array.length - i - 1; j++)
+    	    	{
+    	    	    if(array[j] > array[j + 1]) 
+    	    	    { 
+    	    		    //Array is not sorted as swap was needed
+    	    		    sorted = false;
+
+    	    		    //Swaps array[j + 1] and array[j] 
+    	    		    double temp = array[j]; 
+    	    		    array[j] = array[j + 1]; 
+    	    		    array[j + 1] = temp; 
+    	    	    }
+    	    	}
+    	    }
     	}
     }
     
-	/**
+    /**
      * Sorts the specified array of characters in ascending order using bubble sort.
      * 
      * @param array the unsorted array of characters
      */
     public static void sortChars(char[] array)
     {
-       	if(array != null)
-    	{
-    		boolean sorted = false;
-	        for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
-	        {
-	        	sorted = true;
-	            for(int j = 0; j < array.length - i - 1; j++)
-	            {
-	                if(array[j] > array[j + 1]) 
-	                { 
-	                	//Array is not sorted as swap was needed
-	                	sorted = false;
-	                	
-	                	//Swaps array[j + 1] and array[j] 
-	                    char temp = array[j]; 
-	                    array[j] = array[j + 1]; 
-	                    array[j + 1] = temp; 
-	                }
-	            }
-	        }
+        if(array != null)
+        {
+    	    boolean sorted = false;
+    	    for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
+    	    {
+    	    	sorted = true;
+    	    	for(int j = 0; j < array.length - i - 1; j++)
+    	    	{
+    	    	    if(array[j] > array[j + 1]) 
+    	    	    { 
+    	    		    //Array is not sorted as swap was needed
+    	    		    sorted = false;
+
+    	    		    //Swaps array[j + 1] and array[j] 
+    	    		    char temp = array[j]; 
+    	    		    array[j] = array[j + 1]; 
+    	    		    array[j + 1] = temp; 
+    	    	    }
+    	    	}
+    	    }
     	}
     }
 }
