33d43405a61b4eb2f09b72f8075de97325f2bafb
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-07-24 22:20:44 +0100

    Fixed indentation

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -23,24 +23,24 @@ public class BubbleSort
         if(array != null)
         {
     	    boolean sorted = false;
-	        for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
-	        {
-	            sorted = true;
-	            for(int j = 0; j < array.length - i - 1; j++)
-	            {
-	                if(array[j] > array[j + 1]) 
-	                { 
-	                    //Array is not sorted as swap was needed
-	                    sorted = false;
-	                	
-	                    //Swaps array[j + 1] and array[j] 
-	                    int temp = array[j]; 
-	                    array[j] = array[j + 1]; 
-	                    array[j + 1] = temp; 
-	                }
-	            }
-	        }
-        }
+    	    for(int i = 0; (i < array.length - 1) && (!sorted); i++) 
+    	    {
+    	    	sorted = true;
+    	    	for(int j = 0; j < array.length - i - 1; j++)
+    	    	{
+    	    		if(array[j] > array[j + 1]) 
+    	    	    { 
+    	    			//Array is not sorted as swap was needed
+    	    			sorted = false;
+
+    	    			//Swaps array[j + 1] and array[j] 
+    	    			int temp = array[j]; 
+    	    			array[j] = array[j + 1]; 
+    	    			array[j + 1] = temp; 
+    	    	    }
+    	    	}
+    	    }
+    	}
     }
     
 	/**
