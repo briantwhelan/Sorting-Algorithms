ddf2ab09202f9924dca947dd50bb138214e170fa
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-07-25 16:44:52 +0100

    Reformatted tests and updated Javadoc

--- src/InsertionSortTest.java
+++ src/InsertionSortTest.java
@@ -1,93 +1,81 @@
 import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 /*************************************************************************
- *  Insertion Sort test class.
+ *  {@code InsertionSort} test class.
  *
- *  @version 1.0 11/3/21
+ *  @version 25/7/21
  *
  *  @author Brian Whelan
  *
  *************************************************************************/
 public class InsertionSortTest 
 {
-	/**
-     * Test insertion sort for integers
+    /**
+     * Test {@code sortInts(int[] array)}
      */
     @Test
-    public void testSortingInts()
+    public void testSortInts()
     {
-        //Test null array
     	int[] array = null;
     	InsertionSort.sortInts(array);
-        assertArrayEquals("Testing insertion sort for a null array", null, array);
+        assertArrayEquals("Testing sortInts(int[] array) - Sorting a null array", null, array);
         
-        //Test empty array
         array = new int[] {};
         InsertionSort.sortInts(array);
-        assertArrayEquals("Testing insertion sort for an empty array", new int[] {}, array);
+        assertArrayEquals("Testing sortInts(int[] array) - Sorting an empty array", new int[] {}, array);
         
-        //Test sorted array
         array = new int[] {5, 10, 15, 20};
         InsertionSort.sortInts(array);
-        assertArrayEquals("Testing insertion sort for a sorted array", new int[] {5, 10, 15, 20}, array);
+        assertArrayEquals("Testing sortInts(int[] array) - Sorting a sorted array", new int[] {5, 10, 15, 20}, array);
         
-        //Test unsorted array
         array = new int[] {20, 5, 15, 10};
         InsertionSort.sortInts(array);
-        assertArrayEquals("Testing insertion sort for an unsorted array", new int[] {5, 10, 15, 20}, array);
+        assertArrayEquals("Testing sortInts(int[] array) - Sorting an unsorted array", new int[] {5, 10, 15, 20}, array);
     }
     
-	/**
-     * Test insertion sort for doubles
+    /**
+     * Test {@code sortDoubles(double[] array)}
      */
     @Test
-    public void testSortingDoubles()
+    public void testDoubles()
     {
-        //Test null array
-    	double[] array = null;
-    	InsertionSort.sortDoubles(array);
-        assertArrayEquals("Testing insertion sort for a null array", null, array, 0.0);
+        double[] array = null;
+        InsertionSort.sortDoubles(array);
+        assertArrayEquals("Testing sortDoubles(double[] array) - Sorting a null array", null, array, 0.0);
         
-        //Test empty array
         array = new double[] {};
         InsertionSort.sortDoubles(array);
-        assertArrayEquals("Testing insertion sort for an empty array", new double[] {}, array, 0.0);
+        assertArrayEquals("Testing sortDoubles(double[] array) - Sorting an empty array", new double[] {}, array, 0.0);
         
-        //Test sorted array
         array = new double[] {5.0, 10.0, 15.0, 20.0};
         InsertionSort.sortDoubles(array);
-        assertArrayEquals("Testing insertion sort for a sorted array", new double[] {5.0, 10.0, 15.0, 20.0}, array, 0.0);
+        assertArrayEquals("Testing sortDoubles(double[] array) - Sorting a sorted array", new double[] {5.0, 10.0, 15.0, 20.0}, array, 0.0);
         
-        //Test unsorted array
         array = new double[] {20.0, 5.0, 15.0, 10.0};
         InsertionSort.sortDoubles(array);
-        assertArrayEquals("Testing insertion sort for an unsorted array", new double[] {5.0, 10.0, 15.0, 20.0}, array, 0.0);
+        assertArrayEquals("Testing sortDoubles(double[] array) - Sorting an unsorted array", new double[] {5.0, 10.0, 15.0, 20.0}, array, 0.0);
     }
     
-	/**
-     * Test insertion sort for characters
+    /**
+     * Test {@code sortChars(char[] array)}
      */
     @Test
-    public void testSortingCharacters()
+    public void testChars()
     {
-        //Test null array
-    	char[] array = null;
-    	InsertionSort.sortChars(array);
-        assertArrayEquals("Testing insertion sort for a null array", null, array);
+        char[] array = null;
+        InsertionSort.sortChars(array);
+        assertArrayEquals("Testing sortChars(char[] array) - Sorting a null array", null, array);
         
-        //Test empty array
         array = new char[] {};
         InsertionSort.sortChars(array);
-        assertArrayEquals("Testing insertion sort for an empty array", new char[] {}, array);
+        assertArrayEquals("Testing sortChars(char[] array) - Sorting an empty array", new char[] {}, array);
         
-        //Test sorted array
         array = new char[] {'a', 'b', 'c', 'd', 'e'};
         InsertionSort.sortChars(array);
-        assertArrayEquals("Testing insertion sort for a sorted array", new char[] {'a', 'b', 'c', 'd', 'e'}, array);
+        assertArrayEquals("Testing sortChars(char[] array) - Sorting a sorted array", new char[] {'a', 'b', 'c', 'd', 'e'}, array);
         
-        //Test unsorted array
         array = new char[] {'b', 'r', 'i', 'a', 'n'};
         InsertionSort.sortChars(array);
-        assertArrayEquals("Testing insertion sort for an unsorted array", new char[] {'a', 'b', 'i', 'n', 'r'}, array);
+        assertArrayEquals("Testing sortChars(char[] array) - Sorting an unsorted array", new char[] {'a', 'b', 'i', 'n', 'r'}, array);
     }
 }
