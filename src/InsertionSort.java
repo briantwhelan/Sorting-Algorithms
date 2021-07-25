9aec1db75cf745578924c3cbac53749708fc1a1a
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-07-25 16:40:38 +0100

    Fixed indentation

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -20,22 +20,22 @@ public class InsertionSort
      */
     public static void sortInts(int[] array)
     {
-        if(array != null)
-        { 		
+    	if(array != null)
+    	{ 		
     	    //Inserts each element into correct position of sorted part of array
-	        for(int i = 1; i < array.length; i++)
-	        {   	
-	    	    //Swaps until j reaches start of array or previous element is smaller
-	    	    int j = i;
-	    	    int index = array[i];
-	    	    while((j > 0) && (array[j - 1] > index))
-	    	    {
-	    	        array[j] = array[j - 1];
-	    	        j--;
-	    	    }
-	    	    array[j] = index;
-	    	}
-	    }
+    	    for(int i = 1; i < array.length; i++)
+    	    {   	
+    	        //Swaps until j reaches start of array or previous element is smaller
+    	        int j = i;
+    	        int index = array[i];
+    	        while((j > 0) && (array[j - 1] > index))
+    	        {
+    	            array[j] = array[j - 1];
+    	            j--;
+    	        }
+    	        array[j] = index;
+	        }
+    	}
     }
     
     /**
@@ -57,9 +57,9 @@ public class InsertionSort
     	        {
     	            array[j] = array[j - 1];
     	            j--;
-	    		}
+    	        }
     	        array[j] = index;
-	    	}
+	        }
     	}
     }
     
@@ -69,22 +69,22 @@ public class InsertionSort
      * @param array the unsorted array of characters
      */
     public static void sortChars(char[] array)
-    {
+    {  	
     	if(array != null)
     	{ 		
     	    //Inserts each element into correct position of sorted part of array
     	    for(int i = 1; i < array.length; i++)
-	    	{   	
+    	    {   	
     	        //Swaps until j reaches start of array or previous element is smaller
     	        int j = i;
     	        char index = array[i];
     	        while((j > 0) && (array[j - 1] > index))
-	    		{
+    	        {
     	            array[j] = array[j - 1];
     	            j--;
-	    		}
+    	        }
     	        array[j] = index;
-	    	}
+	        }
     	}
     }
 }
