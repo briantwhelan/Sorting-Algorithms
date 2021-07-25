cbd331ec7db06ea0fe97605193c319bf451ddaf1
briantwhelan <72137130+briantwhelan@users.noreply.github.com>
2021-07-25 21:28:49 +0100

    Fixed indentation

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,93 +1,93 @@
 /*************************************************************************
- *  Shell Sort class.
+ *  {@code ShellSort} class.
  *
- *  @version 1.0 14/3/21
+ *  @version 25/7/21
  *
  *  @author Brian Whelan
  *
  *************************************************************************/
 public class ShellSort 
 {
-	/**
+    /**
      * Don't let anyone instantiate this class.
      */
-	private ShellSort() {}
+    private ShellSort() {}
 	
-	/**
-     * Sort an array of integers using shell sort in ascending order.
+    /**
+     * Sorts the specified array of integers in ascending order using shell sort.
      * 
-     * @param array: an unsorted array of integers.
+     * @param array the unsorted array of integers
      */
     public static void sortInts(int[] array)
     {
-    	if(array != null)
-    	{
-    		for(int gap = array.length/2; gap > 0; gap /= 2) 
-    		{ 
-    			for(int i = gap; i < array.length; i++) 
-    			{ 
-    				//Shift elements until correct array[i] position is found
-    				int temp = array[i]; 
-    				int j; 
-    				for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
-    				{
-    					array[j] = array[j - gap]; 
-    				}
-    				array[j] = temp; 
-    			}
-    		}
-    	}
+        if(array != null)
+        {
+            for(int gap = array.length/2; gap > 0; gap /= 2) 
+            { 
+                for(int i = gap; i < array.length; i++) 
+                { 
+                    //Shifts elements until the correct array[i] position is found
+                    int temp = array[i]; 
+                    int j; 
+                    for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
+                    {
+                        array[j] = array[j - gap]; 
+                    }
+                    array[j] = temp; 
+                }
+            }
+        }
     }
     
-	/**
-     * Sort an array of doubles using shell sort in ascending order.
+    /**
+     * Sorts the specified array of doubles in ascending order using shell sort.
      * 
-     * @param array: an unsorted array of doubles.
+     * @param array the unsorted array of doubles
      */
     public static void sortDoubles(double[] array)
     {
-    	if(array != null)
-    	{
-    		for(int gap = array.length/2; gap > 0; gap /= 2) 
-    		{ 
-    			for(int i = gap; i < array.length; i++) 
-    			{ 
-    				//Shift elements until correct array[i] position is found
-    				double temp = array[i]; 
-    				int j; 
-    				for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
-    				{
-    					array[j] = array[j - gap]; 
-    				}
-    				array[j] = temp; 
-    			}
-    		}
-    	}
+        if(array != null)
+        {
+            for(int gap = array.length/2; gap > 0; gap /= 2) 
+            { 
+                for(int i = gap; i < array.length; i++) 
+                { 
+                    //Shifts elements until the correct array[i] position is found
+                    double temp = array[i]; 
+                    int j; 
+                    for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
+                    {
+                        array[j] = array[j - gap]; 
+                    }
+                    array[j] = temp; 
+                }
+            }
+        }
     }
     
-	/**
-     * Sort an array of characters using shell sort in alphabetical order.
+    /**
+     * Sorts the specified array of characters in alphabetical order using shell sort.
      * 
-     * @param array: an unsorted array of characters.
+     * @param array the unsorted array of characters
      */
     public static void sortChars(char[] array)
     {
-    	if(array != null)
-    	{
-    		for(int gap = array.length/2; gap > 0; gap /= 2) 
-    		{ 
-    			for(int i = gap; i < array.length; i++) 
-    			{ 
-    				//Shift elements until correct array[i] position is found
-    				char temp = array[i]; 
-    				int j; 
-    				for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
-    				{
-    					array[j] = array[j - gap]; 
-    				}
-    				array[j] = temp; 
-    			}
-    		}
-    	}
+        if(array != null)
+        {
+            for(int gap = array.length/2; gap > 0; gap /= 2) 
+            { 
+                for(int i = gap; i < array.length; i++) 
+                { 
+                    //Shifts elements until the correct array[i] position is found
+                    char temp = array[i]; 
+                    int j; 
+                    for(j = i; (j >= gap) && (array[j - gap] > temp); j -= gap) 
+                    {
+                        array[j] = array[j - gap]; 
+                    }
+                    array[j] = temp; 
+                }
+            }
+        }
     }
 }
